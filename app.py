013fe4e8b1dd42861db42333ee9b142213ac9de5
iwurmann7 <157163995+iwurmann7@users.noreply.github.com>
2024-07-09 13:13:38 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+st.title("JobScout")
+st.text(
+    "JobScout is a web application that simplifies job searching by querying multiple job \nlisting sites and saving "
+    "user-defined searches. Utilizing a distributed work \nscheduler, it regularly updates the job listings database "
+    "and notifies users of new \nopportunities. The application also features a user-friendly web interface for "
+    "\nmanual queries and real-time results.")
+
+job_title = st.text_input("Job Title:") # make empty string the default for entire U.S.
+states = [
+    '', 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut',
+    'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa',
+    'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts', 'Michigan',
+    'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+    'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Ohio',
+    'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
+    'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington', 'West Virginia',
+    'Wisconsin', 'Wyoming'
+]
+location = st.selectbox(label="Location:", options=states)
+company = st.text_input("Company:") # can add default value
+
+st.button("search")
+
+
+
+### GET GOOGLE SSO INTEGRATED WITH STREAMLIT - https://discuss.streamlit.io/t/google-authentication-in-a-streamlit-app/43252 ###
